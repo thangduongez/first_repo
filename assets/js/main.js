87f2d2b4631b0db142d6dd357e313574ac4a2dc3
thangduongez <thang.duong@nfq.asia>
2022-05-16 17:57:38 +0700

    brands-section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,34 @@
-const menu_navbar = document.querySelector('.navbar__links')
-const navbar_button = document.querySelector('.navbar_icons')
+const menu_navbar = document.querySelector(".navbar__links");
+const navbar_button = document.querySelector(".navbar_icons");
 
-navbar_button.addEventListener('click', ()=> {
-  menu_navbar.classList.toggle("navbar__open")
-  navbar_button.classList.toggle('open')
-})
+navbar_button.addEventListener("click", () => {
+  menu_navbar.classList.toggle("navbar__open");
+  navbar_button.classList.toggle("open");
+});
+
+const swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 1000,
+  },
+  longSwipesMs: 300,
+  longSwipesRatio: 0.5,
+  animating: true, 
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
