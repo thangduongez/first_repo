11053c4c932c9aaf3f2758e31d77a0c5656d4c9c
thangduongez <thang.duong@nfq.asia>
2022-05-18 18:00:11 +0700

    feate: add responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,37 @@
 const menu_navbar = document.querySelector(".navbar__links");
 const navbar_button = document.querySelector(".navbar_icons");
-
+const btnScroll = document.querySelector(".scroll-top-btn");
+const btnOpenQuestion = document.querySelector(".questions-section__item");
+const iconArrowUp = document.querySelector(".fa-angle-up");
 navbar_button.addEventListener("click", () => {
   menu_navbar.classList.toggle("navbar__open");
   navbar_button.classList.toggle("open");
 });
 
+btnOpenQuestion.addEventListener("click", () => {
+  iconArrowUp.style.transform = "rotate(180deg)";
+});
+
+const topFunction = () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+btnScroll.addEventListener("click", () => {
+  topFunction();
+});
+window.onscroll = () => {
+  // console.log(document.documentElement.scrollTop);
+
+  if (document.documentElement.scrollTop > 200) {
+    btnScroll.classList.add("append_btn");
+  } else {
+    btnScroll.classList.remove("append_btn");
+  }
+};
 const swiper = new Swiper(".swiper", {
   effect: "sidebar",
+  // spaceBetween: 200,
+  autoHeight: true,
   direction: "horizontal",
   loop: true,
 
@@ -16,11 +40,11 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
   },
   autoplay: {
-    delay: 5000,
+    delay: 3000,
   },
   longSwipesMs: 300,
   longSwipesRatio: 0.5,
-  animating: true, 
+  animating: true,
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
@@ -28,7 +52,4 @@ const swiper = new Swiper(".swiper", {
   },
 
   // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
 });
